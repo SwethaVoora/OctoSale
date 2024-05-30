5531ee3e7439bed72f7100ded44d663f88906a39
SwethaVoora <swetha.voora01@gmail.com>
2024-05-30 16:22:38 -0400

    Including vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3000",
         protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "octosale.vercel.app",
       },
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
