bc6dd992f74cd298d3dfe64a9cb01475ea00170b
SwethaVoora <swetha.voora01@gmail.com>
2024-05-29 21:22:17 -0400

    Updated Payload config

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 import * as trpcExpress from "@trpc/server/adapters/express";
 import { appRouter } from "./trpc";
+import basicAuth from "express-basic-auth";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
@@ -19,14 +20,14 @@ const createContext = ({
 const start = async () => {
   // COMMENTING THE PAYLOAD CMS SERVER
   // function where our server/admin dashboard/cms will start
-  // const payload = await getPayloadClient({
-  //   initOptions: {
-  //     express: app,
-  //     onInit: async (cms) => {
-  //       cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
-  //     },
-  //   },
-  // });
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL: ${cms.getAdminURL()}`);
+      },
+    },
+  });
 
   app.use(
     "/api/trpc",
@@ -35,19 +36,42 @@ const start = async () => {
       createContext,
     })
   );
+
+  app.use(
+    "/admin",
+    basicAuth({
+      users: { admin: "password" }, // Replace with your credentials
+      challenge: true,
+    })
+  );
+
   app.use((req, res) => nextHandler(req, res));
 
   nextApp.prepare().then(() => {
-    // payload.logger.info("Next.js started");
+    payload.logger.info("Next.js started");
 
     app.listen(PORT, async () => {
-      // console.log(`Server running at: http://localhost:${PORT}`);
-      console.log(`Next.js App URL : ${process.env.NEXT_PUBLIC_SERVER_URL}`);
-      // payload.logger.info(
-      //   `Next.js App URL : ${process.env.NEXT_PUBLIC_SERVER_URL}`
-      // );
+      payload.logger.info(
+        `Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`
+      );
     });
   });
 };
+
+start();
+//   app.use((req, res) => nextHandler(req, res));
+
+//   nextApp.prepare().then(() => {
+//     // payload.logger.info("Next.js started");
+
+//     app.listen(PORT, async () => {
+//       // console.log(`Server running at: http://localhost:${PORT}`);
+//       console.log(`Next.js App URL : ${process.env.NEXT_PUBLIC_SERVER_URL}`);
+//       // payload.logger.info(
+//       //   `Next.js App URL : ${process.env.NEXT_PUBLIC_SERVER_URL}`
+//       // );
+//     });
+//   });
+// };
 
 start();
